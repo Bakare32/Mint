60e78c96f7abfc60a56c8d5e9a48bf4c09316845
Bakre waris <bakarewariz012@gmail.com>
2022-12-02 00:42:47 +0100

    Create project

---
+++ MintProject/MintProject/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LoginViewController.swift
+//  MintProject
+//
+//  Created by mac on 02/12/2022.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .red
+    }
+    
+
+}
